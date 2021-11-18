c4d956077cece9bc6cb9a4a9b264e69b4f5985d9
liamkronman <liamkronman@gmail.com>
2021-11-18 00:24:45 -0500

    styled splash

--- assets/icons/SuperlativesLogo.js
+++ assets/icons/SuperlativesLogo.js
@@ -1,13 +1,14 @@
 import * as React from "react"
 import Svg, { SvgProps, Path } from "react-native-svg"
 
-function SuperlativesLogo(props: SvgProps) {
+function SuperlativesLogo(props) {
   return (
     <Svg
-      width={39}
-      height={30}
+      width="100%"
+      height="100%"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      viewBox={"0 0 39 30"}
       {...props}
     >
       <Path
--- src/features/auth/screens/Splash.tsx
+++ src/features/auth/screens/Splash.tsx
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Pressable } from 'react-native';
-import PopinButton from 'react-native-popin-button';
+import { View, Text, StyleSheet, Pressable, TouchableOpacity } from 'react-native';
 import SuperlativesLogo from '../../../../assets/icons/SuperlativesLogo.js';
 
 const Splash = ({navigation}) => {
     return (
         <View style={styles.container}>
-            <SuperlativesLogo />
+            <View style={styles.superlativesLogo}>
+                <SuperlativesLogo width={78} height={60} />
+            </View>
             <Text style={styles.superlatives}>
             Superlatives
             </Text>
-            <PopinButton onPress={() => navigation.navigate('Name')}
+            <Text style={styles.subHeader}>
+                What are you known for?
+            </Text>
+            <TouchableOpacity onPress={() => navigation.navigate('Name')}
             style={styles.signupBtn} shrinkTo={0.7}
             >
-                <Text style={styles.signupText}>Sign up</Text>
-            </PopinButton>
+                <Text style={styles.signupText}>Get Started</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.login}>
+                <Text style={styles.loginText}>Log In</Text>
+            </TouchableOpacity>
+            <Text style={styles.footer}>
+                an OuttaControl Production
+            </Text>
         </View>
     )
 }
@@ -27,29 +37,57 @@ const styles = StyleSheet.create({
         backgroundColor: '#16161A',
     },
 
+    superlativesLogo: {
+        width: 78,
+        height: 60,
+        marginBottom: 25,
+    },
+
     superlatives: {
         fontFamily: 'Montserrat-SemiBold',
         color: 'white',
         fontSize: 42,
-        marginBottom: 40,
+        marginBottom: 5,
+    },
+
+    subHeader: {
+        color: '#94A1B2',
+        fontFamily: 'Montserrat',
+        fontSize: 19,
+        marginBottom: 100,
     },
 
     signupBtn: {
         backgroundColor: '#7F5AF0',
         paddingLeft: 80,
         paddingRight: 80,
-        paddingTop: 20,
-        paddingBottom: 20,
+        paddingTop: 15,
+        paddingBottom: 15,
         borderRadius: 6,
         shadowOffset : {height: 4, width: 0},
         shadowOpacity: 0.8,
+        marginBottom: 10,
     },
 
     signupText: {
         fontFamily: 'Montserrat-SemiBold',
         color: 'white',
         fontSize: 23,
-    }
+    },
+
+    loginText: {
+        color: 'white',
+        fontSize: 23,
+        fontFamily: 'Montserrat-SemiBold',
+    },
+
+    footer: {
+        position: 'absolute',
+        bottom: 55,
+        fontSize: 17,
+        color: 'white',
+        fontFamily: 'Montserrat-SemiBold',
+    },
 })
 
 export default Splash;
